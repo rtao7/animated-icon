d372246ed36d1b958e294d44e02649e02d7446c3
rayofsunshine <rtao1014@gmail.com>
2025-01-05 16:07:27 -0800

    fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import { Button } from "@/components/ui/button";
 import { Canvas } from "./components/Canvas";
 import { heroIcons } from "./data/icons/heroIcons";
 import { LucideIcons } from "./data/icons/lucideIcons";
+import { Badge } from "@/components/ui/badge";
 
 function App() {
   return (
-    <div className="bg-[linear-gradient(rgba(243,243,243,0.1),rgba(236,247,252,0.3))]">
-      <div className="isolate group transition-all px-6 h-[60vh] grid place-items-center text-center">
+    <div className="bg-[linear-gradient(180deg,rgba(255,207,196,0.05)_10%,rgba(255,207,196,0.1)_50%,rgba(255,179,240,0.1)_75%,transparent_100%)]">
+      <div className="isolate group transition-all px-6 h-[70vh] grid place-items-center text-center">
         <div className="relative flex flex-col items-center gap-4">
           <Canvas className="absolute group-hover:-top-[100px] group-hover:-rotate-45 group-hover:ease-out group-hover:duration-600 ease-out duration-300 -rotate-[20deg] -top-12 -left-6">
             {heroIcons[0].svg}
@@ -25,18 +26,37 @@ function App() {
           <Canvas className="absolute group-hover:-top-[180px] group-hover:-right-4 group-hover:-rotate-[10deg] group-hover:ease-out group-hover:duration-600 duration-300 rotate-[14deg] -top-20 left-1/2">
             {heroIcons[3].svg}
           </Canvas>
+          <Badge className="rounded-full">Looking for collaborators</Badge>
           <h1 className="z-10 text-4xl font-black font-mono text-transparent bg-[linear-gradient(90deg,rgba(255,179,240,0.5),rgba(61,0,121,1),rgba(61,0,121,1),rgba(110,102,212,0.5))] bg-clip-text">
             Animated icons
           </h1>
-          <p className="z-10 text-base mx-6 md:w-[480px] leading-6 text-muted-foreground">
-            A collection of animated SVG icons - Built on top of Lucid Icons.
-            Download and use freely. Created by Ray.
+          <p className="z-10 font-monoJetBrains text-base mx-6 md:w-[480px] text-muted-foreground">
+            A collection of animated SVG icons - Built using Framer motion and
+            Lucide Icons. Check out Github & Free feel to reuse.
           </p>
-          <Button className="w-fit">Github</Button>
+          <Button variant="secondary" className="w-fit" asChild>
+            <a href="https://github.com/rtao7/animated-icon" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-github"
+              >
+                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                <path d="M9 18c-4.51 2-5-2-7-2" />
+              </svg>
+              Github
+            </a>
+          </Button>
+          <div className="absolute blur-sm -z-10 w-[520px] h-[520px] rounded-full bg-[linear-gradient(rgba(255,207,196,0.50),rgba(255,179,240,0.50))] opacity-50"></div>
         </div>
         {/* Glowing Blur Circle in the Background */}
-        <div className="absolute -z-0 w-[420px] h-[420px] rounded-full bg-[linear-gradient(rgba(255,179,240,0.2),rgba(255,179,240,0.3))] blur-md shadow-lg opacity-20"></div>
-        <div className="absolute blur-md -z-10 w-[520px] h-[520px] rounded-full bg-[linear-gradient(rgba(255,207,196,0.50),rgba(255,179,240,0.50))] opacity-50"></div>
       </div>
       {/* icon gallery container */}
       <div className="relative h-dvh flex flex-col gap-6">
@@ -90,7 +110,8 @@ function App() {
         <div className="h-[1px] w-full bg-[repeating-linear-gradient(90deg,gray,gray_2px,transparent_2px,transparent_4px)] [mask:linear-gradient(90deg,transparent,black_45%,black_55%,transparent)] opacity-30"></div>
       </div>
       {/* Footer */}
-      <div className="relative flex flex-col gap-6">
+      <div className="relative flex flex-col gap-6 items-center h-[40vh] justify-center text-center">
+        <h2 className="z-10 text-2xl font-black font-mono">What is this?</h2>
         <div className="">
           Made by Ray
           <span>
